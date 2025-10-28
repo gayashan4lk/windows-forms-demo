a1654eccd2f5a1e017f8d60db255eec0d3550671
Gayashan Wagachchige BISTEC Global <gayashan@bistecglobal.com>
2025-10-28 19:45:21 +0530

    assignment 2 first commit

---
+++ assignment2/assignment2/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace assignment2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string arrayInput1 = Console.ReadLine();
+            string arrayInput2 = Console.ReadLine();
+
+
+            int[] arr1 = arrayInput1?.Split(',', StringSplitOptions.RemoveEmptyEntries)
+                                   .Select(int.Parse)
+                                   .ToArray() ?? new int[0];
+            int[] arr2 = arrayInput2?.Split(',', StringSplitOptions.RemoveEmptyEntries)
+                                   .Select(int.Parse)
+                                   .ToArray() ?? new int[0];
+
+            //You may use above two arrays that contain inputs.
+            //Complete the below lines to achieve the task.
+        }
+    }
+}
