c7e8546afbe3793a0e0b7b4678f1f84a26ae9a03
Gayashan Wagachchige BISTEC Global <gayashan@bistecglobal.com>
2025-10-28 20:17:40 +0530

    complete

--- assignment2/assignment2/Program.cs
+++ assignment2/assignment2/Program.cs
@@ -17,6 +17,36 @@
 
             //You may use above two arrays that contain inputs.
             //Complete the below lines to achieve the task.
+
+            if (arr1.Length != arr2.Length)
+            {
+                Console.WriteLine("Please enter two number sequences with the same length.");
+                return;
+            }
+
+            int[] gcdResults = new int[arr1.Length];
+
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                var gcd = calculateGCD(arr1 [i], arr2 [i]);
+
+                gcdResults[i] = gcd;
+            }
+
+            Console.WriteLine(string.Join(",", gcdResults));
+        }
+
+        static int calculateGCD(int a, int b)
+        {
+            while (b != 0)
+            {
+                var r = a % b;
+
+                a = b;
+                b = r;
+            }
+
+            return a;
         }
     }
 }
