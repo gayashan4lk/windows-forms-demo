b3d0b7e40263a576b9b0c57afa0e7524f6df2e8e
Gayashan Wagachchige BISTEC Global <gayashan@bistecglobal.com>
2025-10-28 16:13:23 +0530

    group box

--- HelloWorld/HelloWorld/Form1.Designer.cs
+++ HelloWorld/HelloWorld/Form1.Designer.cs
@@ -30,11 +30,13 @@
         {
             this.btnClickThis = new System.Windows.Forms.Button();
             this.lblHelloWorld = new System.Windows.Forms.Label();
+            this.grpboxHelloWorld = new System.Windows.Forms.GroupBox();
+            this.grpboxHelloWorld.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnClickThis
             // 
-            this.btnClickThis.Location = new System.Drawing.Point(87, 59);
+            this.btnClickThis.Location = new System.Drawing.Point(56, 54);
             this.btnClickThis.Name = "btnClickThis";
             this.btnClickThis.Size = new System.Drawing.Size(75, 23);
             this.btnClickThis.TabIndex = 0;
@@ -45,23 +47,33 @@
             // lblHelloWorld
             // 
             this.lblHelloWorld.AutoSize = true;
-            this.lblHelloWorld.Location = new System.Drawing.Point(93, 94);
+            this.lblHelloWorld.Location = new System.Drawing.Point(35, 29);
             this.lblHelloWorld.Name = "lblHelloWorld";
             this.lblHelloWorld.Size = new System.Drawing.Size(116, 13);
             this.lblHelloWorld.TabIndex = 1;
             this.lblHelloWorld.Text = "Please click this button";
+            // 
+            // grpboxHelloWorld
+            // 
+            this.grpboxHelloWorld.Controls.Add(this.btnClickThis);
+            this.grpboxHelloWorld.Controls.Add(this.lblHelloWorld);
+            this.grpboxHelloWorld.Location = new System.Drawing.Point(27, 23);
+            this.grpboxHelloWorld.Name = "grpboxHelloWorld";
+            this.grpboxHelloWorld.Size = new System.Drawing.Size(200, 100);
+            this.grpboxHelloWorld.TabIndex = 2;
+            this.grpboxHelloWorld.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.lblHelloWorld);
-            this.Controls.Add(this.btnClickThis);
+            this.Controls.Add(this.grpboxHelloWorld);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.grpboxHelloWorld.ResumeLayout(false);
+            this.grpboxHelloWorld.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -69,6 +81,7 @@
 
         private System.Windows.Forms.Button btnClickThis;
         private System.Windows.Forms.Label lblHelloWorld;
+        private System.Windows.Forms.GroupBox grpboxHelloWorld;
     }
 }
 
