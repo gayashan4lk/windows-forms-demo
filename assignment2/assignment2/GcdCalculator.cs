33d088d5a49c57f7ccc40d93c5e281f55032b100
Gayashan Wagachchige BISTEC Global <gayashan@bistecglobal.com>
2025-10-28 20:25:35 +0530

    refactor

---
+++ assignment2/assignment2/GcdCalculator.cs
@@ -0,0 +1,34 @@
+﻿namespace assignment2
+{
+    public static class GcdCalculator
+    {
+        public static int CalculateGCD(int a, int b)
+        {
+            while (b != 0)
+            {
+                var r = a % b;
+
+                a = b;
+                b = r;
+            }
+
+            return a;
+        }
+
+        public static int[] CaculateGCDArray(int[] inputA, int[] inputB)
+        {
+            if (inputA.Length != inputB.Length)
+                throw new ArgumentException("InputA and InputB should have the same length");
+
+            int[] gcdResults = new int[inputA.Length];
+
+            for (int i = 0; i < inputA.Length; i++)
+            {
+                var gcd = CalculateGCD(inputA[i], inputB[i]);
+
+                gcdResults[i] = gcd;
+            }
+            return gcdResults;
+        }
+    }
+}
